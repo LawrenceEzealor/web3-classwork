5912591c49ab4e61b5d7594d17970fc03cab3309
Lawrence <lawrence@lawrence.lan>
2024-02-01 12:59:51 +0100

    worked on the placeholder and created a separate div for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,27 +51,31 @@ function App() {
   return (
     //inline css styling
     <div className="App">
-      <h1 className="text-3xl font-bold ">My Message!</h1>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessages(e.target.value)}
-        className="inputfield"
-      />
+      <div className="container mx-auto p-8 bg-red border border-gray-300 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold ">Hello There! 🤓</h1>
+        <div className="w-[467px] m-auto">
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessages(e.target.value)}
+            className="appearance-none block mt-4 w-full bg-gray-200 text-gray-700 border border-gray-300 rounded-md py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 relative"
+            style={{ paddingTop: "1rem" }} // Adjust the value to vertically center the placeholder
+          />
+        </div>
+        <button
+          onClick={setMessage}
+          class="bg-green-500 hover:bg-blue-400 text-white mr-5 ml-5 font-bold py-2 mt-5 px-4 rounded"
+        >
+          set message
+        </button>
 
-      <button
-        onClick={setMessage}
-        class="bg-green-500 hover:bg-blue-400 text-white mr-5 ml-5 font-bold py-2 px-4 rounded"
-      >
-        set message
-      </button>
-
-      <button
-        onClick={getMessage}
-        class="bg-red-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"
-      >
-        get message{" "}
-      </button>
+        <button
+          onClick={getMessage}
+          class="bg-red-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"
+        >
+          get message{" "}
+        </button>
+      </div>
     </div>
   );
 }
